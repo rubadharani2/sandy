64539903dff21493a1017426d9cb3b35e34e63cd
rubadharani2 <30342877+rubadharani2@users.noreply.github.com>
2017-08-11 16:54:50 +0530

    pos.c

---
+++ pos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("Enter a number");
+scanf("%f",&number);
+if(number<=0.0)
+printf("Enter the no is negative num");
+else if(number>0.0)
+printf("Enter the no is positive");
+else
+printf("enter the no is zero");
+return o;
+} 
+
+  
